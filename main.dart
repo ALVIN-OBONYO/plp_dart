382dc13e2eac75d6651fc59073d320dca04cf4dc
ALVIN-OBONYO <alvinnmichael@gmail.com>
2024-03-02 13:34:33 +0300

    plp-dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+// void main() {
+//   // Declaring variables
+//   int num1 = 10;
+//   int num2 = 20;
+//   // double num3 = 30.5;
+
+//   // printing the sum of three numbers
+//   int sum = num1 + num2;
+
+//   print("num1 is $num1");
+//   print("num2 is $num2");
+//   // print("num3 is $num3");
+//   print("sum is $sum");
+// }
+
+void main() {
+  // Declaring values
+  String schoolName = "Powerlearn Project Academy";
+  String address = "Africa";
+
+  print("My school name is $schoolName and the address is $address");
+}
