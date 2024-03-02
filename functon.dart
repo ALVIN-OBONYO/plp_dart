a4509155cc7dc3c81248609646ae2b6a9577d7f0
ALVIN-OBONYO <alvinnmichael@gmail.com>
2024-03-02 17:37:40 +0300

    plp-dart

--- functon.dart
+++ functon.dart
@@ -1,4 +1,4 @@
-void printName() {
+/* void printName() {
   print("My name is Alvin");
 }
 
@@ -6,4 +6,17 @@ void main() {
 
 // calling the function that prints the output
   printName();
+
+}
+*/
+
+void addNum(double num1, int num2) {
+  
+  double sum = num1 + num2;
+
+  print("The sum of $sum");
+}
+
+void main() {
+  addNum(10, 20);
 }
--- map.dart
+++ map.dart
@@ -1,4 +1,4 @@
-void main() {
+ void main() {
   Map<String, int> ages = {"Alice": 22, "Alvin": 27, "Cindy": 20};
 
   print("Ages of students: $ages");
@@ -7,4 +7,5 @@ void main() {
   String rinesString = "Runes in Dart: \u{1f605}";
 
   print(rinesString);
+
 }
