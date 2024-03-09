ea3b303132dfe0827e974ef4b01e224a2a47f940
Alvin Obonyo <41358283+ALVIN-OBONYO@users.noreply.github.com>
2024-03-09 14:51:00 +0300

    condition.dart

---
+++ condition.dart
@@ -0,0 +1,25 @@
+// Control Flow
+
+void main() {
+  var num = 10;
+  if (num > 10) {
+    print("Your number is greater than 10");
+  }
+  if (num < 10) {
+    print("the number is less than 10");
+  }
+  if (num == 10) {
+    print("Your number is equal to 10");
+  }
+}
+
+/*
+void main() {
+  var age = 10;
+  if (age > 18) {
+    print("Alvin can vote");
+  } else {
+    print("Alvin can not vote");
+  }
+}
+*/
